3f5c4e6ba2d7df0df3081bd9583c9cede3a22217
Juliana Nocchi Dobal <junocchi@gmail.com>
2023-09-19 15:05:14 +0100

    Format countVowels

--- count-vowels/src/main.ts
+++ count-vowels/src/main.ts
@@ -1,14 +1,12 @@
-
 type StringOrArray = string | string[];
 
 const countVowels = (x: StringOrArray): number => {
-  
-    if (typeof x === "string") {
-      x = x.split("");
-    }
-  
-  const vowels: string[] = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U" ];
-  return x.filter(char => vowels.includes(char)).length;
+  if (typeof x === "string") {
+    x = x.split("");
+  }
+
+  const vowels: string[] = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+  return x.filter((char) => vowels.includes(char)).length;
 };
 
 console.log(countVowels("abcde")); // => 2
