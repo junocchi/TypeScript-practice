dd4bd97d91110aec05a06e7c732bd426881da615
Juliana Nocchi Dobal <junocchi@gmail.com>
2023-09-19 16:44:09 +0100

    Add NaN option to test

--- literal-type/src/main.ts
+++ literal-type/src/main.ts
@@ -16,6 +16,10 @@ const calculateArea = (shape: Shape, dimension: number) => {
     area = NaN;
   }
 
-  console.log(area);
   return area;
 }; 
+
+console.log(calculateArea("circle", 5)); // 78.53981633974483
+console.log(calculateArea("square", 5)); // 25
+console.log(calculateArea("triangle", 5)); // 10.825317547305483
+console.log(calculateArea("dog", 5)); // NaN
