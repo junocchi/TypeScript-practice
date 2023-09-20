119498408adaf214439cb0a99c3b55b42a8952bf
Juliana Nocchi Dobal <junocchi@gmail.com>
2023-09-20 14:29:34 +0100

    Print list of all Sales employees with the function printEmployeePerDepto

---
+++ read-only-type/build/main.js
@@ -0,0 +1,22 @@
+"use strict";
+var employees = [
+    { name: "Nath", age: 40, department: "Sales", salary: 50600 },
+    { name: "Miguel", age: 35, department: "HR", salary: 45000 },
+    { name: "Rapha", age: 32, department: "Sales", salary: 55700 },
+    { name: "Mari", age: 23, department: "Sales", salary: 48000 },
+    { name: "Lara", age: 30, department: "Sales", salary: 40500 },
+    { name: "Alice", age: 28, department: "HR", salary: 45000 },
+    { name: "Lila", age: 32, department: "Sales", salary: 35400 },
+    { name: "Nico", age: 19, department: "Marketing", salary: 28000 },
+    { name: "Benicio", age: 33, department: "IT", salary: 45400 },
+    { name: "Helena", age: 19, department: "IT", salary: 30000 },
+];
+function printEmployeePerDepto(employees, department) {
+    console.log("Employees in the ".concat(department, " department:"));
+    employees.forEach(function (employee) {
+        if (employee.department === department) {
+            console.log("Name: ".concat(employee.name, ", Age: ").concat(employee.age, ", Salary: \u00A3").concat(employee.salary));
+        }
+    });
+}
+printEmployeePerDepto(employees, "Sales");
--- read-only-type/src/main.ts
+++ read-only-type/src/main.ts
@@ -7,11 +7,35 @@ type Employee = {
 
 const employees: readonly Employee[] = [
   { name: "Nath", age: 40, department: "Sales", salary: 50600 },
-  { name: "Lila", age: 35, department: "HR", salary: 45000 },
+  { name: "Miguel", age: 35, department: "HR", salary: 45000 },
   { name: "Rapha", age: 32, department: "Sales", salary: 55700 },
-  { name: "Mari", age: 23, department: "IT", salary: 48000 },
+  { name: "Mari", age: 23, department: "Sales", salary: 48000 },
   { name: "Lara", age: 30, department: "Sales", salary: 40500 },
   { name: "Alice", age: 28, department: "HR", salary: 45000 },
-  { name: "Miguel", age: 32, department: "Sales", salary: 35400 },
+  { name: "Lila", age: 32, department: "Sales", salary: 35400 },
   { name: "Nico", age: 19, department: "Marketing", salary: 28000 },
+  { name: "Benicio", age: 33, department: "IT", salary: 45400 },
+  { name: "Helena", age: 19, department: "IT", salary: 30000 },
 ];
+
+function printEmployeePerDepto(
+  employees: readonly Employee[],
+  department: string
+): void {
+  console.log(`Employees in the ${department} department:`);
+  employees.forEach((employee) => {
+    if (employee.department === department) {
+      console.log(
+        `Name: ${employee.name}, Age: ${employee.age}, Salary: £${employee.salary}`
+      );
+    }
+  });
+}
+
+printEmployeePerDepto(employees, "Sales");
+// Employees in the Sales department:
+// Name: Nath, Age: 40, Salary: £50600
+// Name: Rapha, Age: 32, Salary: £55700
+// Name: Mari, Age: 23, Salary: £48000
+// Name: Lara, Age: 30, Salary: £40500
+// Name: Lila, Age: 32, Salary: £35400
