a1034c3d311c6f9e10559bb5459e82fef129b7df
Juliana Nocchi Dobal <junocchi@gmail.com>
2023-09-20 10:25:59 +0100

    Add function add passing

---
+++ function-types-exercise/build/main.js
@@ -0,0 +1,3 @@
+"use strict";
+var add = function (num1, num2) { return num1 + num2; };
+console.log(add(15, 30));
---
+++ function-types-exercise/src/main.ts
@@ -0,0 +1,6 @@
+type Operation = (num1: number, num2: number) => number;
+
+const add: Operation = (num1, num2) => num1 + num2;
+
+console.log(add(15,30));
+
