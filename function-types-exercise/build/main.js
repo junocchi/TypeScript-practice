17a1cfc9d7378935fdf5a870368046cd15c71086
Juliana Nocchi Dobal <junocchi@gmail.com>
2023-09-20 10:31:13 +0100

    Add function multuply passing

--- function-types-exercise/build/main.js
+++ function-types-exercise/build/main.js
@@ -2,6 +2,8 @@
 var add = function (num1, num2) { return num1 + num2; };
 var subtract = function (num1, num2) { return num1 - num2; };
 var divide = function (num1, num2) { return num1 / num2; };
+var multiply = function (num1, num2) { return num1 * num2; };
 console.log(add(15, 30)); // 45
 console.log(subtract(155, 55)); // 100
 console.log(divide(100, 5)); // 20
+console.log(multiply(50, 3)); // 150
--- function-types-exercise/src/main.ts
+++ function-types-exercise/src/main.ts
@@ -3,7 +3,9 @@ type Operation = (num1: number, num2: number) => number;
 const add: Operation = (num1, num2) => num1 + num2;
 const subtract: Operation = (num1, num2) => num1 - num2;
 const divide: Operation = (num1, num2) => num1 / num2;
+const multiply: Operation = (num1, num2) => num1 * num2;
 
 console.log(add(15,30)); // 45
 console.log(subtract(155,55)); // 100
 console.log(divide(100,5)); // 20
+console.log(multiply(50,3)); // 150
