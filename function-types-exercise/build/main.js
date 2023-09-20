5c661a582907cc524cb02296bd4b04b17b3fef8e
Juliana Nocchi Dobal <junocchi@gmail.com>
2023-09-20 10:27:58 +0100

    Add function subtract passing

--- function-types-exercise/build/main.js
+++ function-types-exercise/build/main.js
@@ -1,3 +1,5 @@
 "use strict";
 var add = function (num1, num2) { return num1 + num2; };
-console.log(add(15, 30));
+var subtract = function (num1, num2) { return num1 - num2; };
+console.log(add(15, 30)); // 45
+console.log(subtract(155, 55)); // 100
--- function-types-exercise/src/main.ts
+++ function-types-exercise/src/main.ts
@@ -1,6 +1,8 @@
 type Operation = (num1: number, num2: number) => number;
 
 const add: Operation = (num1, num2) => num1 + num2;
+const subtract: Operation = (num1, num2) => num1 - num2;
 
-console.log(add(15,30));
 
+console.log(add(15,30)); // 45
+console.log(subtract(155,55)); // 100
