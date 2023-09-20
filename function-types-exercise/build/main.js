66d9169575b105b309bdb5f7cb8f3fcc8c79d461
Juliana Nocchi Dobal <junocchi@gmail.com>
2023-09-20 11:01:33 +0100

    Add combineOperations passing

--- function-types-exercise/build/main.js
+++ function-types-exercise/build/main.js
@@ -7,3 +7,9 @@ console.log(add(15, 30)); // 45
 console.log(subtract(155, 55)); // 100
 console.log(divide(100, 5)); // 20
 console.log(multiply(50, 3)); // 150
+function combineOperations(choice1, choice2) {
+    return function (num1, num2, num3) { return choice2(choice1(num1, num2), num3); };
+}
+var multiplyThenAdd = combineOperations(multiply, add);
+var result = multiplyThenAdd(5, 3, 7); // (5 * 3) + 7
+console.log(result); // => 22
